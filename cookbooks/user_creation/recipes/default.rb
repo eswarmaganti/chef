503dd681b750ebf050547f74c768b7868cf436b4
Eswar Krishna Maganti <maganti.ek@gmail.com>
2025-02-27 05:52:34 +0530

    Updated cookbook: user_creation

--- cookbooks/user_creation/recipes/default.rb
+++ cookbooks/user_creation/recipes/default.rb
@@ -4,17 +4,29 @@
 #
 # Copyright:: 2025, The Authors, All Rights Reserved.
 
+group_name = node['user_creation']['group_name']
+users = node['user_creation']['users']
+
 # create unix group
-group node['user_creation']['group_name'] do
+group group_name do
   action :create
 end
 
-create unix users
-node['user_creation']['users'].each do |user|
+# create unix users
+users.each do |user|
   user user do
-    group node['user_creation']['group_name']
+    group group_name
     shell '/bin/bash'
     home "/home/#{user}"
+    manage_home true
     action :create
   end
+end
+
+# set password expire policy
+users.each do |user|
+	execute "Set password policy for #{user}" do
+		command "chage --mindays 7 --maxdays 90 --warndays 7 #{user}"
+		action :run
+	end
 end
